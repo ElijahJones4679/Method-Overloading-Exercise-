325dfeb6032ce18b093c462a2dc087ebffb19ca5
elijah jones <jones.elijah4679@gmail.com>
2023-12-23 20:59:23 -0600

    second message

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,3 +2,9 @@
 
 var answer = Methods.Add(2, 2, true);
 Console.WriteLine(answer);
+
+var answer1 = Methods.Add(1, 0, true);
+Console.WriteLine(answer1);
+
+var answer3 = Methods.Add(-9, 2, true);
+Console.WriteLine(answer3);
